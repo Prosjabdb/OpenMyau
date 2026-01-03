922a5e3006c30c3f250698a82af5b7d4fede1709
Prosjabdb <113817586+Prosjabdb@users.noreply.github.com>
2026-01-03 20:59:02 +0530

    Update AutoClicker.java

--- src/main/java/myau/module/modules/AutoClicker.java
+++ src/main/java/myau/module/modules/AutoClicker.java
@@ -4,75 +4,106 @@ import myau.event.EventTarget;
 import myau.event.types.EventType;
 import myau.events.TickEvent;
 import myau.module.Module;
-import myau.property.properties.NumberProperty;
 import myau.property.properties.BooleanProperty;
+import myau.property.properties.IntProperty;
+import myau.util.RandomUtil;
+import myau.util.RotationUtil;
 import net.minecraft.client.Minecraft;
-import java.awt.Robot;
-import java.awt.event.InputEvent;
-import net.minecraft.util.ChatComponentText;
+import net.minecraft.entity.player.EntityPlayer;
 
 public class AutoClicker extends Module {
+
     private static final Minecraft mc = Minecraft.getMinecraft();
 
-    public final NumberProperty clicksPerTick = new NumberProperty("Click Interval (ticks)", 20, 1, 100, 1);
-    public final NumberProperty randomization = new NumberProperty("Randomization (ticks)", 5, 0, 20, 1);
-    public final BooleanProperty disableInGUI = new BooleanProperty("Disable in GUI", true);
-    public final BooleanProperty logClicks = new BooleanProperty("Log Clicks", false);
+    private long clickDelay;
+    private int hitTickTimer = 0;
 
-    private Robot robot;
-    private int tickCount = 0;
-    private int clickCount = 0;
+    public final IntProperty minCPS = new IntProperty("min-cps", 14, 1, 20);
+    public final IntProperty maxCPS = new IntProperty("max-cps", 16, 1, 20);
+
+    public final BooleanProperty timedHit = new BooleanProperty("timed-hit", false);
+    public final IntProperty hitDelayTicks = new IntProperty("hit-delay-ticks", 4, 1, 20, timedHit::getValue);
+
+    public final IntProperty timedMinCPS = new IntProperty("timed-min-cps", 6, 1, 20, timedHit::getValue);
+    public final IntProperty timedMaxCPS = new IntProperty("timed-max-cps", 10, 1, 20, timedHit::getValue);
 
     public AutoClicker() {
         super("AutoClicker", false);
-        try {
-            robot = new Robot();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    }
+
+    private boolean isValidTarget(EntityPlayer player) {
+        if (player == mc.thePlayer || player.isDead) return false;
+        float eyeHeight = player.getEyeHeight();
+        return RotationUtil.rayTrace(
+                player.getEntityBoundingBox().expand(0.2, eyeHeight + 0.1, 0.2),
+                mc.thePlayer.rotationYaw,
+                mc.thePlayer.rotationPitch,
+                3.0F
+        ) != null;
+    }
+
+    private EntityPlayer getClosestTarget() {
+        return mc.theWorld.playerEntities.stream()
+                .filter(this::isValidTarget)
+                .min((a, b) -> Float.compare(
+                        mc.thePlayer.getDistanceToEntity(a),
+                        mc.thePlayer.getDistanceToEntity(b)
+                )).orElse(null);
+    }
+
+    private long getNextClickDelay() {
+        boolean timed = timedHit.getValue() && hitTickTimer > 0;
+        int min = timed ? timedMinCPS.getValue() : minCPS.getValue();
+        int max = timed ? timedMaxCPS.getValue() : maxCPS.getValue();
+        return 1000L / RandomUtil.nextLong(min, max);
     }
 
     @EventTarget
     public void onTick(TickEvent event) {
-        if (!this.isEnabled() || event.getType() != EventType.PRE) return;
-        if (mc.theWorld == null || mc.thePlayer == null) return;
-        if (disableInGUI.getValue() && mc.currentScreen != null) return;
+        if (event.getType() != EventType.PRE) return;
 
-        tickCount++;
+        if (clickDelay > 0) clickDelay -= 50;
 
-        int effectiveInterval = clicksPerTick.getValue().intValue();
-
-        // Add randomization
-        if (randomization.getValue().intValue() > 0) {
-            effectiveInterval += (int) (Math.random() * randomization.getValue().intValue());
+        if (mc.currentScreen != null) {
+            return;
         }
 
-        if (tickCount >= effectiveInterval) {
-            click();
-            tickCount = 0;
+        EntityPlayer target = getClosestTarget();
+
+        if (timedHit.getValue() && target != null) {
+            float dist = mc.thePlayer.getDistanceToEntity(target);
+            if (dist <= 3.0F) hitTickTimer = hitDelayTicks.getValue();
         }
-    }
 
-    private void click() {
-        if (robot == null) return;
+        if (hitTickTimer > 0) hitTickTimer--;
 
-        robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-        robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        if (!isEnabled()) return;
+        if (!mc.gameSettings.keyBindAttack.isKeyDown()) return;
+        if (mc.thePlayer.isUsingItem()) return;
 
-        clickCount++;
-        if (logClicks.getValue() && clickCount % 10 == 0) { // log every 10 clicks
-            mc.thePlayer.addChatMessage(new ChatComponentText("AutoClicker: Clicked " + clickCount + " times"));
+        while (clickDelay <= 0) {
+            clickDelay += getNextClickDelay();
+
+            // Attack the closest target if exists
+            if (target != null) {
+                mc.playerController.attackEntity(mc.thePlayer, target);
+            }
+
+            // Swing hand visually
+            mc.thePlayer.swingItem();
         }
     }
 
     @Override
-    public void onDisabled() {
-        tickCount = 0;
-        clickCount = 0;
+    public void onEnabled() {
+        clickDelay = 0;
+        hitTickTimer = 0;
     }
 
     @Override
     public String[] getSuffix() {
-        return new String[]{clicksPerTick.getValue().intValue() + " ticks"};
+        return minCPS.getValue().equals(maxCPS.getValue())
+                ? new String[]{minCPS.getValue().toString()}
+                : new String[]{String.format("%d-%d", minCPS.getValue(), maxCPS.getValue())};
     }
 }
